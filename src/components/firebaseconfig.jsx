a489199dcfbf3480469d6e13f53a7e9fea6660ba
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2024-03-01 17:36:30 +0530

    Update firebaseconfig.jsx

--- src/components/firebaseconfig.jsx
+++ src/components/firebaseconfig.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX0YuPxqpttPs5J5_YPxkcYKf3dLNZLEU",
-  authDomain: "asvintech-46912.firebaseapp.com",
-  projectId: "asvintech-46912",
-  storageBucket: "asvintech-46912.appspot.com",
-  messagingSenderId: "87111723230",
-  appId: "1:87111723230:web:1fb19ba2b54ca320f03a4f",
-  measurementId: "G-HE6MESSFSZ"
+  apiKey: "AIzaSyB5fiSq6r0fMRrLOwjbGqohHiPppLC4WI0",
+  authDomain: "backasvin.firebaseapp.com",
+  projectId: "backasvin",
+  storageBucket: "backasvin.appspot.com",
+  messagingSenderId: "6226011431",
+  appId: "1:6226011431:web:ccd9b6730cc1eaf3517502",
+  measurementId: "G-BLF2CHE1LJ"
 };
 
 const app = initializeApp(firebaseConfig);
